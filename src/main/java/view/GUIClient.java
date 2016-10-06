9870fdc3b7c24101777de63075bfc45b9501e3ff
c13slk <c13slk@cs.umu.se>
2016-10-06 19:09:29 +0200

    client GUI

--- src/main/java/view/GUIClient.java
+++ src/main/java/view/GUIClient.java
@@ -1,6 +1,8 @@
 package view;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by c13slk on 2016-09-29.
@@ -12,11 +14,32 @@ public class GUIClient {
     private JTextField textField1;
     private JTextArea textArea2;
     private JButton sendButton;
-    private JTextField textField2;
     private JButton selectGroupButton;
     private JTextField groupNameTextField;
     private JRadioButton basicNonReliableRadioButton;
     private JRadioButton basicReliableRadioButton;
     private JRadioButton treeBasedReliableRadioButton;
     private JButton addGroupButton;
+    private JTextField usernameTextField;
+    private JButton deleteGroupButton;
+    private JTextArea textArea3;
+
+    private JPanel chatt = new JPanel();
+
+
+
+    public GUIClient() {
+        addGroupButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tabbedPane1.addTab("hej", panel1);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
 }
