5631041d2655cc0c5a32a155a0e509acbd704f4a
Timmy Olsson <c12ton@cs.umu.se>
2016-10-08 14:44:23 +0200

    Can create group trough gcom

---
+++ src/test/java/gcom/groupmodule/GroupMemberTest.java
@@ -0,0 +1,29 @@
+package gcom.groupmodule;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by c12ton on 2016-10-08.
+ */
+public class GroupMemberTest {
+
+    @Before
+    public void setUp() throws Exception {
+        // set up fake group, that user is a member of
+
+    }
+
+    @Test
+    public void testMemberJoins() {
+
+    }
+
+    @Test
+    public void testMemberLeaves() {
+
+    }
+
+}
