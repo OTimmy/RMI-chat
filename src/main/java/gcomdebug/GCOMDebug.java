f54f19691b010f28f5e7a5202a62d85337e819d5
Timmy Olsson <c12ton@cs.umu.se>
2016-10-12 14:22:02 +0200

    Fixed stupid error

--- src/main/java/gcomdebug/GCOMDebug.java
+++ src/main/java/gcomdebug/GCOMDebug.java
@@ -2,8 +2,14 @@ package gcomdebug;
 
 import gcom.AbstractGCOM;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 /**
  * Created by c12ton on 10/12/16.
  */
 public class GCOMDebug extends AbstractGCOM{
+    public GCOMDebug(String host) throws RemoteException, NotBoundException {
+        super(host);
+    }
 }
