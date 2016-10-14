eadb088142dd2d7030e36db22cbfdad26222a05f
c13slk <c13slk@cs.umu.se>
2016-10-14 16:43:06 +0200

    debug gui added view

--- src/main/java/gcomdebug/Controller/DebugController.java
+++ src/main/java/gcomdebug/Controller/DebugController.java
@@ -2,12 +2,15 @@ package gcomdebug.Controller;
 
 import gcomdebug.view.DebugClient;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by c12ton on 10/12/16.
  */
 public class DebugController {
 
-    private DebugClient gui;
+    private static DebugClient gui;
 
     public DebugController(DebugClient gui) {
         this.gui = gui;
@@ -15,7 +18,19 @@ public class DebugController {
 
     public static void main(String[] args){
 
-        DebugController controller = new DebugController(new DebugClient());
+        DebugController controller;
+
+        controller = new DebugController(new DebugClient());
+
+        gui.addListenerToDs(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
 
     }
+
+
 }
--- src/main/java/gcomdebug/view/DebugClient.java
+++ src/main/java/gcomdebug/view/DebugClient.java
@@ -3,6 +3,8 @@ package gcomdebug.view;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.security.PublicKey;
 
 /**
  * Created by c13slk on 2016-10-14.
@@ -10,16 +12,22 @@ import java.awt.*;
 public class DebugClient {
 
     private JFrame debugFrame = new JFrame("Debug");
+    private DefaultTableModel groupsModel = new DefaultTableModel();
+    private DefaultTableModel incommingModel = new DefaultTableModel();
+    private DefaultTableModel outgoingModel = new DefaultTableModel();
+    private DefaultTableModel vectorModel = new DefaultTableModel();
+
+    private JButton rs = new JButton("Release selected");
+    private JButton ds = new JButton("Drop selected");
+    private JButton ra = new JButton("Release all");
+
+    private JTable debugGroups;
 
     public DebugClient() {
 
-        DefaultTableModel groupsModel = new DefaultTableModel();
-        DefaultTableModel messModel = new DefaultTableModel();
-        DefaultTableModel vectorModel = new DefaultTableModel();
-
         JPanel debugPane = new JPanel(new BorderLayout());
 
-        JTable debugGroups = new JTable(groupsModel) {
+        debugGroups = new JTable(groupsModel) {
             @Override
             public boolean isCellEditable(int row, int column) {
                 return false;
@@ -39,20 +47,53 @@ public class DebugClient {
         groupPanel.add(groupsSp);
         groupPanel.add(refreshButton);
 
+        //--------------------------------------------------------------------->
 
-        JButton hold = new JButton("Hold messages");
-        JPanel button = new JPanel();
-        button.add(hold);
 
-        JTable messTable = new JTable(messModel) {
+        JPanel buttons = new JPanel();
+        buttons.add(rs);
+        buttons.add(ds);
+        buttons.add(ra);
+
+        JTable incommingTable = new JTable(incommingModel) {
             @Override
             public boolean isCellEditable(int row, int column) {
                 return false;
             }
         };
-        messModel.addColumn("Member");
-        messModel.addColumn("Message");
-        JScrollPane scrollMessPane = new JScrollPane(messTable);
+        incommingModel.addColumn("Member");
+        incommingModel.addColumn("Message");
+        JScrollPane scrollIncommingPane = new JScrollPane(incommingTable);
+
+        JPanel incWL = new JPanel();
+        incWL.setLayout(new BoxLayout(incWL, BoxLayout.Y_AXIS));
+        JLabel incLabel = new JLabel("Incomming");
+        incLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        incWL.add(incLabel);
+        incWL.add(scrollIncommingPane);
+
+        JTable outgoingTable = new JTable(outgoingModel) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        outgoingModel.addColumn("Member");
+        outgoingModel.addColumn("Message");
+        JScrollPane scrollOutgoingPane = new JScrollPane(outgoingTable);
+
+        JPanel outWL = new JPanel();
+        outWL.setLayout(new BoxLayout(outWL, BoxLayout.Y_AXIS));
+        JLabel outLabel = new JLabel("Outgoing");
+        outLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        outWL.add(outLabel);
+        outWL.add(scrollOutgoingPane);
+
+        JPanel incOutPane = new JPanel();
+        incOutPane.setLayout(new BoxLayout(incOutPane, BoxLayout.X_AXIS));
+        incOutPane.add(incWL);
+        incOutPane.add(outWL);
+
 
         JTable vectorTable = new JTable(vectorModel) {
             @Override
@@ -66,8 +107,8 @@ public class DebugClient {
 
         JPanel tableBPane = new JPanel();
         tableBPane.setLayout(new BoxLayout(tableBPane, BoxLayout.Y_AXIS));
-        tableBPane.add(button);
-        tableBPane.add(scrollMessPane);
+        tableBPane.add(incOutPane);
+        tableBPane.add(buttons);
         tableBPane.add(scrollvectorPane);
 
         debugPane.add(tableBPane, BorderLayout.WEST);
@@ -80,4 +121,59 @@ public class DebugClient {
         debugFrame.pack();
         debugFrame.setVisible(true);
     }
+
+    public void addIncomming(String member, String message){
+        incommingModel.addRow(new Object[]{member, message});
+
+    }
+
+    public void removeFromIncomming(int row){
+        incommingModel.removeRow(row);
+    }
+
+    public void addOutgoing(String member, String message){
+        outgoingModel.addRow(new Object[]{member, message});
+
+    }
+
+    public void removeFromOutgoing(int row){
+        outgoingModel.removeRow(row);
+
+    }
+
+    public void addVector(){
+
+    }
+
+    public void removeVector(){
+
+    }
+
+    public void updateVector(){
+
+    }
+
+    public void updateDebugGroups(String[] groups) {
+        debugGroups.clearSelection();
+
+        for (int i = debugGroups.getRowCount() - 1; i >= 0; i--) {
+            groupsModel.removeRow(i);
+        }
+
+        for (String group : groups) {
+            groupsModel.addRow(new Object[]{group});
+        }
+    }
+
+    public void addListenerToRs(ActionListener a){
+        rs.addActionListener(a);
+    }
+    public void addListenerToDs(ActionListener a){
+        ds.addActionListener(a);
+    }
+    public void addListenerToRa(ActionListener a){
+        ra.addActionListener(a);
+    }
+
+
 }
