6c71aed78d7b20e78a193063cf40e1031824cab3
c13slk <c13slk@cs.umu.se>
2016-10-19 18:33:30 +0200

    error

--- src/main/java/client/controller/Controller.java
+++ src/main/java/client/controller/Controller.java
@@ -172,7 +172,7 @@ public class Controller {
                 try {
                     members = gcom.connectToGroup(data[1], data[0]);
                 } catch (GCOMException e1) {
-                    gui.showErrorMess("Connection refused! \nYou cannot join the group right now.");
+                    gui.showErrorMess("Error: " + e1.getMessage());
                     return;
                 }
 
