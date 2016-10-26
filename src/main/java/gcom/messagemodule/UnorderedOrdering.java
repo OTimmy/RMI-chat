a6e91e7301d5a888ee99e426cad80d7f69fbd285
Timmy Olsson <c12ton@cs.umu.se>
2016-10-26 11:00:37 +0200

    Fixed unorded debug bug problem

--- src/main/java/gcom/messagemodule/UnorderedOrdering.java
+++ src/main/java/gcom/messagemodule/UnorderedOrdering.java
@@ -12,12 +12,26 @@ public class UnorderedOrdering implements Ordering {
 
     private String name;
     private Message message;
+    private HashMap<String,Integer> vectorClock;
     public UnorderedOrdering(String name) {
         this.name = name;
+        vectorClock = new HashMap<>();
+        vectorClock.put(name,0);
     }
 
     @Override
-    public void setMessageStamp(Message message, String[] names){}
+    public void setMessageStamp(Message message, String[] names){
+        for(String name:names) {
+            if(!vectorClock.containsKey(name)) {
+            }
+        }
+
+        try {
+            message.setVectorClock((HashMap<String, Integer>) vectorClock.clone());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+     }
 
     @Override
     public Message[] orderMessage(Message m) {
